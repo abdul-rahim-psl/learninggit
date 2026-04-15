c6adec581216bfe28bb967a9a16f6eab46533178
abdul-rahim-psl <abdul.rahim@paysyslabs.com>
2026-04-15 11:36:38 +0500

    feat: added comment
    Signed-off-by: abdul-rahim-psl abdul.rahim@paysyslabs.com

--- a.ts
+++ a.ts
@@ -1,7 +1,7 @@
 function add(a: number, b: number): number {
-    return a + b;
+    return a + b; // i have added this comment here
 }
 
 function subtract(a: number, b: number): number {
-    return a - b;
+    return a - b; // i ave added this comment here as well
 }
